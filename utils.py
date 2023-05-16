f18aa0b2c3253161e9c85c4fbfe68659528f0604
Haniye Kosariyan <haniye.kosariyan@gmail.com>
2023-05-16 17:43:47 +0430

    Add docstring to function

--- utils.py
+++ utils.py
@@ -1,11 +1,23 @@
 def add(a,b):
+   """
+   This function adds tow numbers
+   """
    return a + b
 
 def sub(a,b):
+   """
+   This function subtract tow numbers
+   """
    return a - b
 
 def mul(a,b):
+   """
+   This function multiplies tow numbers
+   """
    return a * b
 
 def div(a,b):
+   """
+   This function devision tow numbers
+   """
    return a / b
